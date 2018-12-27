de571a98dcbcdab0c03af3c7e229880f97a367be
Luzan Baral <luzanbaral@gmail.com>
2018-12-27 13:14:29 +0545

    Lab 5 group B solutions

---
+++ Lab/5/B/program1.c
@@ -0,0 +1,42 @@
+/**
+
+Write a program to read values of 3x3 order matrix
+then compute the sum of even elements and sum of odd elements.
+
+**/
+#include<stdio.h>
+#define SIZE 3
+int main(){
+	int array[SIZE][SIZE];
+	int esum = 0, osum = 0, i, j;
+	
+	for(i=0; i<=SIZE-1; i++){
+		for(j=0; j<=SIZE-1; j++) {
+			printf("Enter element for position [%d][%d] of array: ", i, j);
+			scanf("%d", &array[i][j]);
+		}
+	}
+	
+	printf("\nYour matrix \n");
+	for(i=0; i<=SIZE-1; i++){
+		for(j=0; j<=SIZE-1; j++) {
+			printf("%d\t", array[i][j]);
+		}
+		printf("\n");
+	}
+	
+	//calculating sum
+	for(i=0; i<=SIZE-1; i++){
+		for(j=0; j<=SIZE-1; j++) {
+			if(array[i][j] % 2 == 0)
+        		esum += array[i][j];
+    		else
+		        osum += array[i][j];
+		}
+	}
+	
+	printf("\nSum of even numbers from Matrix is %d", esum);
+	printf("\nSum of odd numbers from Matrix is %d", osum);
+	
+	return 0;
+}
---
+++ Lab/5/B/readme.md
@@ -0,0 +1,12 @@
+## LAB 4 - Group B
+
+1. Write a program to read values of 3x3 order matrix then computer the sum of even elements and sum of odd elements.
+
+2. Write a program to check whether the given string is palindrome or not without using string functions.
+   > Palindrome is a word which reads same from left to right and right to left.
+   > For e.g. LIRIL, MADAM, etc.
+
+## Task Completion
+
+- [x] Question 1 - Solution in `program1.c` file
+- [x] Question 2 - Solution in `program2.c` file
