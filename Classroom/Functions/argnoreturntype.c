d35cc9f109bce056c483412ae9c2205c9bb0fbe6
Luzan Baral <luzanbaral@gmail.com>
2018-12-30 15:56:37 +0545

    Function Examples added

---
+++ Classroom/Functions/argnoreturntype.c
@@ -0,0 +1,38 @@
+/**
+Function Category: Function with argument but no return value
+
+This example shows that the function does not return any value but does take some argument.
+**/
+#include <stdio.h>
+void checkPrimeAndDisplay(int n);
+
+int main()
+{
+    int n;
+
+    printf("Enter a positive integer: ");
+    scanf("%d",&n);
+
+    // n is passed to the function
+    checkPrimeAndDisplay(n);
+
+    return 0;
+}
+
+// void indicates that no value is returned from the function
+void checkPrimeAndDisplay(int n)
+{
+    int i, flag = 0;
+
+    for(i=2; i <= n/2; ++i)
+    {
+        if(n%i == 0){
+            flag = 1;
+            break;
+        }
+    }
+    if(flag == 1)
+        printf("%d is not a prime number.",n);
+    else
+        printf("%d is a prime number.", n);
+}
---
+++ Classroom/Functions/argumentwithreturn.c
@@ -0,0 +1,41 @@
+/**
+Function Category: Function that has argument and also returns value
+
+This example shows that the function has two way data communication,
+i.e. it takes some values as argument and also returns value.
+**/
+#include <stdio.h>
+int checkPrimeNumber(int n);
+
+int main()
+{
+    int n, flag;
+
+    printf("Enter a positive integer: ");
+    scanf("%d",&n);
+
+    // n is passed to the checkPrimeNumber() function
+    // the value returned from the function is assigned to flag variable
+    flag = checkPrimeNumber(n);
+
+    if(flag == 1)
+        printf("%d is not a prime number",n);
+    else
+        printf("%d is a prime number",n);
+
+    return 0;
+}
+
+// integer is returned from the function
+int checkPrimeNumber(int n)
+{
+    int i;
+
+    for(i=2; i <= n/2; ++i)
+    {
+        if(n%i == 0)
+            return 1;
+    }
+
+    return 0;
+}
