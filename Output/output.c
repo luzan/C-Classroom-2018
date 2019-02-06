e39d92d579ad4ae3ef65442281b5e29c5e04a49d
Luzan Baral <luzanbaral@gmail.com>
2019-02-06 12:12:41 +0545

    modified

--- Output/output.c
+++ Output/output.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 
 int main(){
-	int x = 16, y = 18, z;
-	
-	y += x++; 
-	z = ++x + y++; 
-	x = x + y + z--; 
+	int x = 18, y = 16, z;
+
+	y += x++;
+	z = ++x + y++;
+	x = x + y + z--;
 	printf("\n x = %d\ny = %d\nz = %d", x,y,z);
 	return 0;
 }
